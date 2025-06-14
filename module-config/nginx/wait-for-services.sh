b0a3357ce9cc5228fb473fd7a3c3e8acaf83fbe1
Hadi ABDALLAH <hadi.abdallah@intalio.com>
2025-06-14 12:09:13 +0200

    change

--- module-config/nginx/wait-for-services.sh
+++ module-config/nginx/wait-for-services.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-# wait for dependent services to be reachable before starting Nginx
+# Wait for a host:port to be reachable
 wait_for() {
   host="$1"
   port="$2"
   echo "Waiting for $host:$port..."
-  until bash -c '</dev/tcp/$host/$port' 2>/dev/null; do
+
+  while ! nc -z "$host" "$port" >/dev/null 2>&1; do
     sleep 2
   done
+
+  echo "$host:$port is available."
 }
 
+# Wait for Keycloak
 wait_for keycloak 8080
 
+# Start NGINX
 exec nginx -g 'daemon off;'
