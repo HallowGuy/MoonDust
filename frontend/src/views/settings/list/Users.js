e677a578a573fee5440fb9a7181caf185a8cbd9a
Max97436 <maxime.delormema@gmail.com>
2025-09-02 16:36:38 +0400

    Push V2 permission

--- frontend/src/views/settings/list/Users.js
+++ frontend/src/views/settings/list/Users.js
@@ -229,6 +229,8 @@ const { actionsConfig, currentUserRoles } = useContext(PermissionsContext)
         <CCardHeader className="d-flex justify-content-between align-items-center">
           <h2 className="mb-0">Utilisateurs</h2>
            <ProtectedButton
+            actionsConfig={actionsConfig}
+  currentUserRoles={currentUserRoles}
   action="user.new"
 >
           <CButton color="primary" onClick={() => openOffcanvas()}>
