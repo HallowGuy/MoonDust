4962ff46df509b900853767bc7cc1a78f8e2bf1a
Max97436 <maxime.delormema@gmail.com>
2025-09-03 10:21:05 +0400

    Permission boutons Users

--- frontend/src/components/ProtectedButton.js
+++ frontend/src/components/ProtectedButton.js
@@ -1,21 +1,14 @@
-import React from "react"
+import React, { useEffect, useState,useContext } from "react"
+import { PermissionsContext } from "src/context/PermissionsContext"
+const ProtectedButton = ({ action, children }) => {
+  const { actionsConfig, currentUserRoles } = useContext(PermissionsContext)
 
-const ProtectedButton = ({ action, actionsConfig, currentUserRoles, children }) => {
   if (!actionsConfig || !currentUserRoles) return null
 
-  // 🔎 Rôles autorisés depuis le JSON
   const allowedRoles = (actionsConfig[action] || []).map(r => r.toLowerCase())
-
-  // 🔎 Rôles de l'utilisateur normalisés
   const userRoles = currentUserRoles.map(r => r.toLowerCase())
 
-  console.log(`🛡️ Action: ${action}`)
-  console.log("👉 AllowedRoles:", allowedRoles)
-  console.log("👉 UserRoles:", userRoles)
-
-  // ✅ Vérifie si au moins un rôle correspond
   const canAccess = userRoles.some(role => allowedRoles.includes(role))
-
   if (!canAccess) return null
 
   return <>{children}</>
